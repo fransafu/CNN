b12e0f0b5fd25064733564b5410226bb72ebd891
Francisco Javier Sanchez Fuentes <francisco.sanchez@sr-consultores.com>
2020-10-25 21:19:23 -0300

    disable callbacks and checkpoints

--- train.py
+++ train.py
@@ -23,11 +23,6 @@ def read_arguments():
     parser.add_argument("-model", type=str, help=" name of model (resNet or others)", choices = ['resnet', 'alexnet'], required = True)              
     return parser.parse_args()
 
-def create_tfrecords(config):
-    dataset = Dataset(config)
-    dataset.create_tfrecords('train', config.image_type)
-    dataset.create_tfrecords('test', config.image_type)
-
 def load_dataset(config):
     # Generate
     tfr_train_file = os.path.join(config.dataset_dir, "train.tfrecords")
@@ -42,6 +37,7 @@ def load_dataset(config):
 
     return tfr_train_file, tfr_test_file, mean_file, shape_file
 
+"""
 def find_latest_ckp(ckp_path):
     latest_ckp = None
     for file in os.listdir(ckp_path):
@@ -53,6 +49,7 @@ def find_latest_ckp(ckp_path):
             else:
                 latest_ckp = new_version
     return latest_ckp
+"""
 
 def save_history(history_filename, history):
     with open(history_filename, 'wb') as file_pi:
@@ -65,10 +62,7 @@ def main():
     config = Config()
     config.load(Path(pargs.config))
 
-    # Dataset: Generate tfrecords
-    # create_tfrecords(config)   
-
-    # Dataset: Load
+    # DATASET SECTION
     tfr_train_file, tfr_test_file, mean_file, shape_file = load_dataset(config)
 
     input_shape = np.fromfile(shape_file, dtype=np.int32)
@@ -77,87 +71,83 @@ def main():
 
     number_of_classes = config.num_classes
 
-    # loading tfrecords into dataset object
+    # Loading tfrecords into dataset object
     tr_dataset = tf.data.TFRecordDataset(tfr_train_file)
     tr_dataset = tr_dataset.map(lambda x : TFRecord.parser_tfrecord(x, input_shape, mean_image, number_of_classes, with_augmentation = True));    
     tr_dataset = tr_dataset.shuffle(config.shuffle_size)
     tr_dataset = tr_dataset.batch(batch_size = config.batch_size)
-    # tr_dataset = tr_dataset.repeat()
 
-    print(len(tfr_test_file))
     val_dataset = tf.data.TFRecordDataset(tfr_test_file)
     val_dataset = val_dataset.map(lambda x : TFRecord.parser_tfrecord(x, input_shape, mean_image, number_of_classes, with_augmentation = False));    
     val_dataset = val_dataset.batch(batch_size = config.batch_size)
 
-    print(val_dataset.element_spec)
-    print(tf.size(val_dataset))
-
+    # TF SESSION SECTION
     # This code allows program to run in  multiple GPUs. It was tested with 2 gpus.
     tf.debugging.set_log_device_placement(True)
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
-        checkpoint_path = "cp-{epoch:04d}.ckpt"
-        checkpoint_dir = f"{config.snapshot_dir}/{checkpoint_path}"
+        # CALLBACK SECTION
 
-        # callback    
-        # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=config.snapshot_dir, histogram_freq=1)
-        logdir = os.path.join("logs", datetime.now().strftime("%Y%m%d-%H%M%S"))
-        tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+        # checkpoint_path = "cp-{epoch:04d}.ckpt"
+        # checkpoint_dir = f"{config.snapshot_dir}/{checkpoint_path}"
+
+        # callback
+        # logdir = os.path.join("logs", datetime.now().strftime("%Y%m%d-%H%M%S"))
+        # tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
         # Defining callback for saving checkpoints
         # save_freq: frecuency in terms of number steps each time checkpoint is saved 
 
-        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath=checkpoint_dir, # config.snapshot_dir + '{epoch:03d}.h5',
-            save_weights_only=True,
-            verbose=1,
-            mode = 'max',
-            monitor='val_acc',
-            save_freq = 'epoch',            
-        )
+        # model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        #     filepath=checkpoint_dir, # config.snapshot_dir + '{epoch:03d}.h5',
+        #     save_weights_only=True,
+        #     verbose=1,
+        #     mode = 'max',
+        #     monitor='val_acc',
+        #     save_freq = 'epoch',            
+        # )
 
-        # save_freq = configuration.get_snapshot_steps())
+        # MODEL SELECTION SECTION
         model = None
         if model_name == 'alexnet':
-            print('Model is alexnet')
             model = AlexNetModel(config.num_classes)
         else:
             # Resnet 34
-            print('Model is Resnet')
             model = resnet.ResNet([3,4,6,3], [64,128,256,512], config.num_classes, se_factor = 0)
 
-        print("\n------ input_image -------\n")
-        #build the model indicating the input shape
-        #define the model input
-        print((input_shape[0], input_shape[1], input_shape[2]))
+        print(f"""
+
+            Input image, shape: ({input_shape[0]}, {input_shape[1]}, {input_shape[2]})
+            Model: {model_name}
+
+        """)
+
         input_image = tf.keras.Input((input_shape[0], input_shape[1], input_shape[2]), name = 'input_image')     
-        print("\n------ model(input_image) -------\n")
         model(input_image)
-        print("\n------- model.summary() ------\n")
         model.summary()
 
+        # CHECKPOINT SECTION
         # Use_checkpoints to load weights
-        if config.use_checkpoint and find_latest_ckp(config.snapshot_dir):
-            #latest = tf.train.latest_checkpoint(config.snapshot_dir)
-            #print(latest)
-            print(config.use_checkpoint)
-            print(find_latest_ckp(config.snapshot_dir))
-            ckp_filepath = config.snapshot_dir + '{epoch:03d}.h5'.format(epoch=find_latest_ckp(config.snapshot_dir))
-            print(ckp_filepath)
-            model.load_weights(ckp_filepath, by_name = True, skip_mismatch = True)
-        else:
-            model.save_weights(checkpoint_path.format(epoch=0))
+        # if config.use_checkpoint and find_latest_ckp(config.snapshot_dir):
+        #     #latest = tf.train.latest_checkpoint(config.snapshot_dir)
+        #     #print(latest)
+        #     print(config.use_checkpoint)
+        #     print(find_latest_ckp(config.snapshot_dir))
+        #     ckp_filepath = config.snapshot_dir + '{epoch:03d}.h5'.format(epoch=find_latest_ckp(config.snapshot_dir))
+        #     print(ckp_filepath)
+        #     model.load_weights(ckp_filepath, by_name = True, skip_mismatch = True)
+        # else:
+        #     model.save_weights(checkpoint_path.format(epoch=0))
 
-        # Definin optimizer, my experince shows that SGD + cosine decay is a good starting point        
-        # Recommended learning_rate is 0.1, and decay_steps = total_number_of_steps                        
-        initial_learning_rate= config.learning_rate
-        lr_schedule = tf.keras.experimental.CosineDecay(initial_learning_rate = initial_learning_rate,
-                                                        decay_steps = config.weight_decay,
-                                                        alpha = 0.0001)
-
+        # OPTIMIZER SECTION
         opt = None
         if config.optimizer_name == 'adam':
             opt = tf.keras.optimizers.Adam(learning_rate = config.learning_rate)
         elif config.optimizer_name == 'sgd':
+            # Definin optimizer, my experince shows that SGD + cosine decay is a good starting point        
+            # Recommended learning_rate is 0.1, and decay_steps = total_number_of_steps                        
+            lr_schedule = tf.keras.experimental.CosineDecay(initial_learning_rate = config.learning_rate,
+                                                            decay_steps = config.weight_decay,
+                                                            alpha = 0.0001)
             opt = tf.keras.optimizers.SGD(learning_rate = lr_schedule, momentum = 0.9, nesterov = True)        
 
         model.compile(optimizer=opt, loss=losses.crossentropy_loss, metrics=['accuracy'])        
@@ -165,11 +155,9 @@ def main():
         history = model.fit(tr_dataset, 
                         epochs = config.num_epochs,
                         validation_data=val_dataset,
-                        validation_steps = float(config.validation_steps),
-                        callbacks=[model_checkpoint_callback, tensorboard_callback]
+                        validation_steps = float(config.validation_steps)# ,
+                        # callbacks=[model_checkpoint_callback, tensorboard_callback]
         )
-
-        # model.evaluate(val_dataset, steps = config.validation_steps, callbacks=[tensorboard_callback])                                
 
         # Save history
         save_history(f"{model_name}_history", history)
