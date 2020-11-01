437b6700c30b341f4298cd19312c25ba4a3919de
Francisco Javier Sanchez Fuentes <francisco.sanchez@sr-consultores.com>
2020-11-01 13:53:46 -0300

    fix filename undefined

--- infrastructure/Save.py
+++ infrastructure/Save.py
@@ -21,8 +21,8 @@ class Save:
         self.basepath_experiment = f"{self.config.basepath_dir}/experiments"
         self.timestamp = datetime.now().strftime('%d%m%Y%H%M%S')
 
-    def __save_history(self, history) -> str:
-        filename_history = f"{self.timestamp}_{self.config.model_name}_history"
+    def __save_history(self, history, model_filename) -> str:
+        filename_history = f"{self.timestamp}_{model_filename}_history"
         with open(filename_history, 'wb') as history_file:
             pickle.dump(history.history, history_file)
         return filename_history
@@ -30,7 +30,7 @@ class Save:
     def experiment(self, history, model_filename, config_filepath) -> None:
         name_zipfile = f"{self.timestamp}_{model_filename}.zip"
 
-        history_filename = self.__save_history(history)
+        history_filename = self.__save_history(history, model_filename)
 
         zipfile = Zip(self.basepath_experiment, name_zipfile)
         zipfile.zipdir(model_filename)
