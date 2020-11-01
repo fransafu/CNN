f2b7119ab26162fdf3752043138435e441a1beb6
Francisco Javier Sanchez Fuentes <francisco.sanchez@sr-consultores.com>
2020-11-01 12:35:42 -0300

    refactor: better names for private method and close file after use

--- infrastructure/Save.py
+++ infrastructure/Save.py
@@ -21,18 +21,19 @@ class Save:
         self.basepath_experiment = f"{self.config.basepath_dir}/experiments"
         self.timestamp = datetime.now().strftime('%d%m%Y%H%M%S')
 
-    def __write_history(self, history) -> str:
+    def __save_history(self, history) -> str:
         filename_history = f"{self.timestamp}_{self.config.model_name}_history"
         with open(filename_history, 'wb') as history_file:
             pickle.dump(history.history, history_file)
         return filename_history
 
-    def experiment(self, history, model_pathname, config_filename) -> None:
-        name_zipfile = f"{self.timestamp}.zip"
+    def experiment(self, history, model_filename, config_filepath) -> None:
+        name_zipfile = f"{self.timestamp}_{model_filename}.zip"
 
-        history_filename = self.__write_history(history)
+        history_filename = self.__save_history(history)
 
         zipfile = Zip(self.basepath_experiment, name_zipfile)
-        zipfile.zipdir(model_pathname)
+        zipfile.zipdir(model_filename)
         zipfile.zipfile(history_filename)
-        zipfile.zipfile(config_filename)
+        zipfile.zipfile(config_filepath)
+        zipfile.close()
